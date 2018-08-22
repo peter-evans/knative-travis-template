0c1c0f81d2b933a8bfb850b99623233bda39d94c
peter-evans <peter-evans@users.noreply.github.com>
2018-08-22 10:43:29 +0900

    Add build_app script

---
+++ .ci/build_app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+docker build -t helloworld-go .
+
+kubectl apply -f service.yml
+
+kubectl get pods --show-labels
+
+# Wait for helloworld-go to be ready
+echo "Waiting for helloworld-go to be ready ..."
+for i in {1..30}; do # Timeout after 5 minutes
+  kubectl get pods --show-labels
+  #if kubectl get pods --namespace=istio-system -listio=pilot|grep Running ; then
+  #  break
+  #fi
+  sleep 2
+done
+
+export IP_ADDRESS=$(kubectl get node -o 'jsonpath={.items[0].status.addresses[0].address}'):$(kubectl get svc knative-ingressgateway -n istio-system -o 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
+export HOST_URL=$(kubectl get services.serving.knative.dev helloworld-go -o jsonpath='{.status.domain}')
+
+echo $IP_ADDRESS
+echo $HOST_URL
